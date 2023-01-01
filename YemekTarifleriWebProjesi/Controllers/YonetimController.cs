a307f9a355a595e2280d7f6829fa5604d76bf46e
mahmutegribck <104262911+mahmutegribck@users.noreply.github.com>
2023-01-01 15:14:55 +0300

    WebProgramlama

--- YemekTarifleriWebProjesi/Controllers/YonetimController.cs
+++ YemekTarifleriWebProjesi/Controllers/YonetimController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.ConstrainedExecution;
+using YemekTarifleriWebProjesi.Models;
 
 namespace YemekTarifleriWebProjesi.Controllers
 {
     public class YonetimController : Controller
     {
+        YemektarifleriDbContext db = new YemektarifleriDbContext();
         public IActionResult Index()
         {
             return View();
@@ -21,6 +24,43 @@ namespace YemekTarifleriWebProjesi.Controllers
             return View();
         }
         public IActionResult Bilgilerim()
+        {
+            return View();
+        }
+        public IActionResult Sayfalar()
+        {
+            var sayfalar = db.Sayfalars.Where(s => s.Silindi == false).OrderBy(s => s.Baslik).ToList();
+
+            return View (sayfalar);
+
+        }
+        public IActionResult SayfaEkle()
+        {
+            
+
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult SayfaEkle(Sayfalar sayfa)
+        {
+            sayfa.Silindi = false;
+            db.Sayfalars.Add(sayfa);
+            db.SaveChanges();
+
+            return RedirectToAction("Sayfalar");
+
+        }
+
+        public IActionResult SayfaGetir(int id)
+        {
+            var sayfa = db.Sayfalars.Where(s => s.Silindi == false && s.SayfaId == id).FirstOrDefault();
+
+            return View("SayfaGuncelle",sayfa);
+
+        }
+
+        public IActionResult CikisYap()
         {
             return View();
         }
